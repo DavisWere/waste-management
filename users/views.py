7cb2f16350c21bcaac899fcbe86c2566197c2f4a
Davisetims <etimsintegration@gmail.com>
2025-07-09 08:17:21 +0300

    add login function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            # Redirect based on user type
+            if user.user_type == "resident":
+                return redirect("resident-dashboard")
+            elif user.user_type == "garbage_collector": 
+                return redirect("collector-dashboard")
+            elif user.is_superuser:
+                return redirect("admin-dashboard")
+            else:
+                messages.error(request, "Unauthorized access")
+                return redirect("index")  
+
+        else:
+            messages.error(request, "Invalid username or password")
+
+    return render(request, "login.html")
+
+
+def logout_view(request):
+    logout(request)  
+    return redirect('/')

68bf24c56d917b8fc022b4958eaff0a0e5b4c981
Davisetims <etimsintegration@gmail.com>
2025-07-12 10:00:19 +0300

    `Added User model registration and user type field to admin interface`

--- users/models.py
+++ users/models.py
@@ -21,7 +21,7 @@ class User(AbstractUser):
         null=True
     )
     email = models.EmailField(unique=True)
-
+    user_type = models.CharField(max_length=20, choices=USER_TYPES, default='resident')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
